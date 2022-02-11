31d3dd22631693a31be7dd6a8b1abf588f10fa27
Simone823 <simone.daglio823@outlook.it>
2022-02-12 00:51:34 +0100

    somma numeri inseriti dall'utente

--- JSnack3/app.js
+++ JSnack3/app.js
@@ -7,7 +7,7 @@ console.log(numbers);
 while (numbers.length !== 5){
 
    // Chiedo all'utente un numero  
-   const numbersUser = parseInt(prompt("digita un numero!"));
+   const numbersUser = parseInt(prompt("digita un numero!").trim());
 
    // Se l'utente digita un numero, aggiungo il numero dentro l'array numbers
    if (!isNaN(numbersUser)){
@@ -15,6 +15,17 @@ while (numbers.length !== 5){
    }
 }
 console.log(numbers);
+
+
+// Prendo tutti i valori all'interno dell'array numbers e le inserisco in una variabile
+const [numeroUno, numeroDue, numeroTre, numeroQuattro, numeroCinque] = numbers;
+console.log(numeroUno, numeroDue, numeroTre, numeroQuattro, numeroCinque);
+
+
+// Somma numeri inseriti dall'utente
+const result = numeroUno + numeroDue + numeroTre + numeroQuattro + numeroCinque;
+console.log(result);
+
 
 
 
