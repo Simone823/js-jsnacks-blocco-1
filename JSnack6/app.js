77e95b9f451aa2144cc0e994a18bf463a8d1b3ac
Simone823 <simone.daglio823@outlook.it>
2022-02-12 13:45:55 +0100

    cubo dei primi numeri inseriti dall'utente

--- JSnack6/app.js
+++ JSnack6/app.js
@@ -2,11 +2,13 @@
 const number = [];
 console.log(number);
 
+let numberUser;
+
 // Finche la lunghezza del mio array number è 0, chiederò all'utente di inserire un numero
 while (number.length !== 1) {
 
     // Chiedo all'utente di inserire un numero
-    const numberUser = parseInt(prompt("Digita un numero"));
+    numberUser = parseInt(prompt("Digita un numero"));
     console.log(numberUser);
 
     // Se l'utente non inserisce un numero, quindi una stringa
@@ -21,3 +23,26 @@ while (number.length !== 1) {
 
 }
 console.log(number);
+
+// Recupero div container dal dom
+const containerHtml = document.querySelector(".container");
+console.log(containerHtml);
+
+for (let i = 1; i <= numberUser; i++) {
+
+    const cubeNumbers = i ** 3;
+    console.log(cubeNumbers);
+
+    // Creo un div contenente il cubo dei primi N inseriti dall'utente
+    const divCube = document.createElement("div");
+    divCube.classList.add(`cube${i}-${numberUser}`);
+    console.log(divCube);
+
+    // Appendo il mio divcube dentro containerHtml
+    containerHtml.append(divCube);
+
+    // Inserisco all'interno di divcube il cubo dei primi numeri inseriti
+    divCube.append(cubeNumbers);
+    console.log(divCube);
+}
+console.log(containerHtml);
