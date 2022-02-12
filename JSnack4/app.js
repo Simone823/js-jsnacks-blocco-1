976b457604e653f92680dc16a571cc2d38c06c21
Simone823 <simone.daglio823@outlook.it>
2022-02-12 10:11:08 +0100

    ciclo for guestlist, guestcheck

--- JSnack4/app.js
+++ JSnack4/app.js
@@ -12,3 +12,26 @@ let userName =  prompt("Come ti chiami").trim();
 console.log(userName);
 
 
+// Ciclo for guestList
+for (let i = 0; i < guestList.length; i++){
+   
+    console.log(guestList[i]);
+
+    guestCheck = guestList[i];
+    console.log(guestCheck);
+
+    // Se il nome inserito dall'utente è in lista guestList allora può partecipare alla festa
+    if (userName == guestCheck){
+        console.log("Puoi partecipare alla festa");
+        alert("Puoi partecipare alla festa");
+        break;
+    } 
+
+}
+
+
+// Se l'utente non è in lista non potrà partecipare alla festa 
+if(userName != guestCheck){
+    console.log("Non puoi partecipare alla festa");
+    alert("Non puoi partecipare alla festa");
+} 
