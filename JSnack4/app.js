d34501d6056e1ee00bab658ee2094d3c54c6bb99
Simone823 <simone.daglio823@outlook.it>
2022-02-12 01:41:34 +0100

    lista invitati, chiedo all'utente il suo nome

---
+++ JSnack4/app.js
@@ -0,0 +1,14 @@
+// Creo la lista delle persone invitate alla festa
+const guestList = ["Paolo", "Luca", "Gigi", "Mario", "Francesco"];
+console.log(guestList); 
+
+
+// Creo una varibile per la verifica del nome 
+var guestCheck;
+console.log(guestCheck);
+
+// Chiedo all'utente il suo nome
+let userName =  prompt("Come ti chiami").trim();
+console.log(userName);
+
+
