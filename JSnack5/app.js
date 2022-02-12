1e3c5ad00344ea448a5078cc174104f9c1152bab
Simone823 <simone.daglio823@outlook.it>
2022-02-12 10:55:49 +0100

    se dispari lo aggiungo nell'array vuoto

--- JSnack5/app.js
+++ JSnack5/app.js
@@ -9,5 +9,13 @@ while (numbers.length !== 6)
     // Chiedo all'utente di inserire un numero
     const userNumber = parseInt(prompt("Digita un numero"));
     console.log(userNumber);
+
+    // Se l'utente digita un numero dispari allora lo inserirò nell'array vuoto numbers
+    if (userNumber % 2 == 1){
+        numbers.push(userNumber);
+    } // Se l'utente digita un numero pari, il numero digitato non sarà valido 
+      else {
+      alert("Numeri non validi");
+    }
 }
 console.log(numbers);
